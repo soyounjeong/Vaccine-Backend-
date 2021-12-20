f8ac06ab2855ca93b13b620b35936c9a3f4c86a6
jaesung <jaesung190@gmail.com>
2021-12-20 15:37:54 +0900

    커밍

---
+++ frontend/bsgogo/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link,BrowserRouter as Router } from "react-router-dom";
+import styled from 'styled-components';
+const LoginPage = () => {
+    return(
+        <div className="body">
+            <p>되냐?</p>
+        </div>
+    )
+}
+export default LoginPage;
